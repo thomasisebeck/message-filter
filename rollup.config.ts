f10dc4b1e359567d6234888f263fcbb553c42a34
thomasisebeck <tisebeck@proton.me>
2026-01-02 12:28:45 +0200

    created a release

---
+++ rollup.config.ts
@@ -0,0 +1,17 @@
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+
+export default {
+  input: "src/index.ts", // Entry point for the bundle (main TypeScript file)
+  output: {
+    file: "dist/index.js", // Output file location and name
+    format: "es", // Output format: ES module
+    sourcemap: false, // Generate sourcemaps for easier debugging
+  },
+  plugins: [
+    terser(), // Use terser for minification to make the bundle smaller
+    typescript({
+      tsconfig: "./tsconfig.json", // Use this tsconfig file to configure TypeScript compilation
+    }),
+  ],
+};
